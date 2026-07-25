4c4797ad2a595dee33e647aa9de7ca139bd71c86
ShashidharaShashi <hvshashidhar@gmail.com>
2026-07-25 13:19:59 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Mail, Phone, Linkedin, MapPin, Award, ChevronRight, Sparkles, Server, Cpu, Shield, FileText, Camera, Upload } from 'lucide-react';
 import { contactData, yearsOfExp } from '../data/resumeData';
-import myPhoto from '../assets/Shashidhara h v.jpeg';
+import myPhoto from '../assets/Shashidhara h v.jpeg.JPG';
 
 interface HeroProps {
   onOpenPrintModal: () => void;
